5a4285d02b7f17d89719f28a84de9ebc7a336f1b
Leon <mail@leonlan.com>
2018-11-09 12:23:46 +0100

    Initial commit

---
+++ helperfunctions.py
@@ -0,0 +1,45 @@
+from math import ceil, sqrt
+"""
+Prime numbers
+	* prime(n)
+		A number n is prime if:
+			- It is not even.
+			- It does not have a divisor smaller than sqrt(n).
+			- From the above conditions, we can exclude even divisors.
+	* 
+
+"""
+
+def prime(n):
+	"""Returns whether n is prime or not."""
+	
+	# Case 1: n is even
+	if n % 2 == 0:
+		return False
+
+	# Case 2: n is odd	
+	for i in range(3, ceil(sqrt(n)), 2):
+		if n % i == 0:
+			return False
+
+	return True
+	
+def primelist(n, length=False):
+	"""Returns the list of primes up to n. 
+	If length = TRUE, return the first n prime numbers."""
+	primes = [2]
+	
+	# Case 1: all primes up to n.
+	if not length:
+		for i in range(3, n+1, 2):
+			if prime(i):
+				primes.append(i)
+
+	# Case 2: the first n prime numbers.
+	else:
+		j = 3
+		while len(primes) < n:
+			if prime(j):
+				primes.append(j)
+			j += 1
+	return(primes)
