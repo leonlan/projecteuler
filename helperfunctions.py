5ed07c29eb26bc4ec0202220ac6f0b9e7c9c73db
Leon <mail@leonlan.com>
2018-12-16 15:33:41 +0100

    Added problem 21.

--- helperfunctions.py
+++ helperfunctions.py
@@ -6,7 +6,7 @@ Prime numbers
 			- It is not even.
 			- It does not have a divisor smaller than sqrt(n).
 			- From the above conditions, we can exclude even divisors.
-	* 
+	*
 
 """
 
@@ -15,7 +15,7 @@ def prime(n):
 	# Case 0: n is 0, 1 or negative
 	if n < 2:
 		return False
-	
+
 	# Case 1: n = 2
 	elif n == 2:
 		return True
@@ -24,22 +24,22 @@ def prime(n):
 	elif n % 2 == 0:
 		return False
 
-	# Case 2: n is odd	
+	# Case 2: n is odd
 	for i in range(3, ceil(sqrt(n))+1, 2):
 		if n % i == 0:
 			return False
 
 	return True
-	
+
 def primelist(n, start=2, length=False):
-	"""Returns the list of primes up to n (starting from start=3). 
+	"""Returns the list of primes up to n (starting from start=3).
 	If length = TRUE, return the first n prime numbers."""
 	# Separate case for 2 since it is even
 	if start > 2:
 		primes = []
 	else:
 		primes = [2]
-	
+
 	# Primes cannot be even
 	if start % 2 == 0:
 		start += 1
@@ -69,12 +69,12 @@ def properdivisors(n):
 			propdiv.extend([i, n//i])
 
 	# If n is a perfect square, also add the square root.
-	# Note: this does not work for VERY LARGE n. 
+	# Note: this does not work for VERY LARGE n.
 	if sqrt(n).is_integer() and n != 1:
 		propdiv.append(int(sqrt(n)))
-	
+
 	return(propdiv)
-	
+
 
 
 """
@@ -89,4 +89,3 @@ def sort(n, integer=False):
         return(int(x))
     else:
         return(x)
-
