529f2c24c978d5fd4d964eb565d0e6380bf8c3ca
Leon <mail@leonlan.com>
2020-07-17 14:05:38 +0200

    Tab indent changes

--- go/p003.go
+++ go/p003.go
@@ -6,18 +6,18 @@ import . "./euler"
 func max(numbers map[int]int) int {
     var maxNumber int
     for n := range numbers {
-		maxNumber = n
-		break
+        maxNumber = n
+        break
     }
     for n := range numbers {
 		if n > maxNumber {
-			maxNumber = n
-		}
+            maxNumber = n
+        }
     }
     return maxNumber
 }
 
 func main() {
-	pf := PrimeFactorization(600851475143)
-	fmt.Println(max(pf))
+    pf := PrimeFactorization(600851475143)
+    fmt.Println(max(pf))
 }
